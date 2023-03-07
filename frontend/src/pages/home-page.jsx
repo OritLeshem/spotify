21fc9f1d29d6e74c22c7bbfd2887eb90a83bc604
OritLeshem <azmono@gmail.com>
2023-03-07 11:16:35 -0500

    play-btn, setup css

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import triangle from '../assets/imgs/triangle.svg'
+import { GreenBtn } from '../cmps/form'
 
 export function HomePage() {
 
     return <section className="home-page">
         <h1>hello from home page!</h1>
+        <GreenBtn />
     </section >
 }
