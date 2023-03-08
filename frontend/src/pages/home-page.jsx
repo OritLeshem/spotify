35fa12ed4a6849184dd1ff302e3caf6653545491
MayanFeldbaum <mayan.lemberg@gmail.com>
2023-03-08 13:53:28 +0200

    try

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -4,6 +4,7 @@ import { GreenBtn } from '../cmps/form'
 
 export function HomePage() {
 
+    // home page
     return <section className="home-page">
         <h1>hello from home page!</h1>
         <GreenBtn />
